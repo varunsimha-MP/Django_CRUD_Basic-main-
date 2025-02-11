f842a43c852a951094279c7e1cd1572d8843bbd1
Varunsimha M P <77954226+varunsimha-MP@users.noreply.github.com>
2025-02-12 02:28:06 +0530

    settings.py

--- basic/settings.py
+++ basic/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'basic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_crud',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
